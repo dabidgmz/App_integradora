a3d4909bc71adb85bbdf0e3886908b769a573362
anie05rv7 <anie05rv7@gmail.com>
2023-11-15 22:00:20 -0600

    menu, todavia falta terminarlo

--- app/src/main/java/com/example/app_integradora/Menu.java
+++ app/src/main/java/com/example/app_integradora/Menu.java
@@ -1,5 +1,30 @@
 package com.example.app_integradora;
 
-public class Menu {
-    private String prueba;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Menu extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        final  DrawerLayout drawerLayout = findViewById(R.id.menu);
+        findViewById(R.id.imagMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+
+    }
+
+
+
+
+
 }
