c0b6c02eb9e6bb8f78e55da7fe2b75bfaa830184
dabidgmz <david.gmzherrera28@gmail.com>
2023-11-15 20:40:56 -0600

    se agrego register_empresa y diseño de User_Register

--- app/src/main/java/com/example/app_integradora/User_Register.java
+++ app/src/main/java/com/example/app_integradora/User_Register.java
@@ -2,7 +2,10 @@ package com.example.app_integradora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class User_Register extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class User_Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+        Button buttonRegistrarUsuario=findViewById(R.id.buttonRegistrarUsuario);
+        buttonRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_Register.this,register_empresa.class);
+                startActivity(intent);
+            }
+        });
     }
 }
