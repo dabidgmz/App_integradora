dc77a29a95433c89c95ebb60dd8c6eff24dbeece
dabidgmz <david.gmzherrera28@gmail.com>
2023-11-15 21:47:51 -0600

    se agrego register_empresa y diseño de User_Register

---
+++ app/src/main/java/com/example/app_integradora/pantalla_principal.java
@@ -0,0 +1,14 @@
+package com.example.app_integradora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantalla_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal);
+    }
+}
