29c668da155a8fc87bb84e70ffb2730c13e5f830
anie05rv7 <anie05rv7@gmail.com>
2023-11-16 19:47:54 -0600

    se agrego el subMenu

--- app/src/main/java/com/example/app_integradora/Menu.java
+++ app/src/main/java/com/example/app_integradora/Menu.java
@@ -1,30 +1,66 @@
 package com.example.app_integradora;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 public class Menu extends AppCompatActivity {
     @Override
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        final  DrawerLayout drawerLayout = findViewById(R.id.menu);
+        final DrawerLayout drawerLayout = findViewById(R.id.menu);
         findViewById(R.id.imagMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
-
             }
         });
-
     }
 
+  /* public void onMenuOptionSelected(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.menuEmpresa1:
+                intent = new Intent(this, InformacionEmpresaActivity.class);
+                intent.putExtra("nombre_empresa", "Empresa 1");
+                startActivity(intent);
+                break;
 
+            case R.id.menuAgregarEmpresa:
+                intent = new Intent(this, AgregarEmpresaActivity.class);
+                startActivity(intent);
+                break;
 
+            case R.id.menuVitrina1:
+                intent = new Intent(this, InformacionVitrinaActivity.class);
+                intent.putExtra("nombre_vitrina", "Vitrina 1");
+                startActivity(intent);
+                break;
 
+            case R.id.menuAgregarVitrina:
+                intent = new Intent(this, AgregarVitrinaActivity.class);
+                startActivity(intent);
+                break;
 
+            case R.id.menuSensor1:
+                intent = new Intent(this, InformacionSensorActivity.class);
+                intent.putExtra("nombre_sensor", "Sensor 1");
+                startActivity(intent);
+                break;
+
+            case R.id.menuAgregarSensor:
+                intent = new Intent(this, AgregarSensorActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }*/
 }
+
+
+
+
+
