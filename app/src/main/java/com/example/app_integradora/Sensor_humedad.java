ee6cd8b9ba59c7ae81efc20a03b5cce9de8dba80
Jesus Ramirez <jesusal.rg.03@gmail.com>
2023-11-21 09:54:27 -0600

    agrege sensores 3

---
+++ app/src/main/java/com/example/app_integradora/Sensor_humedad.java
@@ -0,0 +1,14 @@
+package com.example.app_integradora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sensor_humedad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_humedad);
+    }
+}
---
+++ app/src/main/java/com/example/app_integradora/Sensor_impacto.java
@@ -0,0 +1,14 @@
+package com.example.app_integradora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sensor_impacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_impacto);
+    }
+}
