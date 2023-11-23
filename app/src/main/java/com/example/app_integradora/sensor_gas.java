23d5c23dc3c2b8555f1e6b0d3baa12637427c62b
dabidgmz <david.gmzherrera28@gmail.com>
2023-11-23 12:08:54 -0600

    la verdad

--- app/src/main/java/com/example/app_integradora/sensor_gas.java
+++ app/src/main/java/com/example/app_integradora/sensor_gas.java
@@ -1,7 +1,7 @@
 package com.example.app_integradora;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import com.github.mikephil.charting.charts.LineChart
 import android.os.Bundle;
 
 public class sensor_gas extends AppCompatActivity {
