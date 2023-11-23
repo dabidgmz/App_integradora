3b182b49837a2d2762e4706bdaf2d034b67b0c4f
anie05rv7 <anie05rv7@gmail.com>
2023-11-23 13:08:22 -0600

    cambios

---
+++ app/src/main/java/com/example/app_integradora/sensor_luz.java
@@ -0,0 +1,14 @@
+package com.example.app_integradora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sensor_luz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_luz);
+    }
+}
